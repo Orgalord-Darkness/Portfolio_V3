e980735579253cbf0ae5c5a405fea870a55febf7
Orgalord-Darkness <heddy.mameri@gmail.com>
2025-04-13 14:37:18 +0200

    Correction bug projets_has_apprentissages

--- database/migrations/2025_04_13_123617_create_projets_has_apprentissages_table.php
+++ database/migrations/2025_04_13_123617_create_projets_has_apprentissages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjetHasApprentissageTable extends Migration
+class CreateProjetsHasApprentissagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -33,3 +33,4 @@ class CreateProjetHasApprentissageTable extends Migration
         Schema::dropIfExists('projets_has_apprentissages');
     }
 }
+
