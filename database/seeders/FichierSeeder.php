aab689009ebd7af011454b779a203d461a1cee32
Orgalord-Darkness <heddy.mameri@gmail.com>
2025-04-16 08:29:14 +0200

    Création FichierSeeder

---
+++ database/seeders/FichierSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FichierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('fichiers')->insert([
+            [
+                
+            ]
+            ]);
+    }
+}
